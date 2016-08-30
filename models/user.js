151a3c4511561fa83f49c0ee4a050d19addf2c26
Arshin Jain <arshin.jain@outlook.com>
2016-08-30 12:49:14 -0700

    Comments ++

--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+// User Model
+
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 
@@ -28,7 +30,9 @@ var UserSchema = new mongoose.Schema({
 });
 
 // pre save method
+UserSchema.pre('save', function(next) {
 
+});
 
 var User = mongoose.model('User', UserSchema);
 module.exports = User;
