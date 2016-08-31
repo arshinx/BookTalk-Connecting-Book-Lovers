710b71233f17d9b555fead1d8d7a7b3372ae4551
Arshin Jain <arshin.jain@outlook.com>
2016-08-31 16:10:12 -0700

    Use locals to share userId in templates

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.use(session({
   saveUninitialized: false
 }));
 
+// Allow User ID Access in Templates
+app.use(function(req, res, next){
+  res.locals.currentUser = req.session.userId;
+  next();
+});
+
 // MongoDB Connection
 mongoose.connect("mongodb://localhost:27017/bookworm");
 var db = mongoose.connection;
