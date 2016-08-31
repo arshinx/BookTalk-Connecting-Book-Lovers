ef54663f77322659eca50b39519a8ddfea13cab5
Arshin Jain <arshin.jain@outlook.com>
2016-08-31 11:36:26 -0700

    Profile Page Template

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,13 @@ router.get('/', function(req, res, next) {
   return res.render('index', { title: 'Home' });
 });
 
+// GET /profile
+router.get('/profile', function(req, res, next){
+  if (! req.session.userId) {
+    var err = new Error("You are not authorized to view this page.");
+  }
+});
+
 // GET /login
 router.get('/login', function(req, res, next) {
   return res.render('login', {title: 'Log In'});
