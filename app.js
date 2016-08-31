5d3a2faae70ea4d690a44db5e6a062b2ac1e0999
Arshin Jain <arshin.jain@outlook.com>
2016-08-31 10:08:34 -0700

    Require Express Session Middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var session = require('express-session');
 var app = express();
 
 // MongoDB Connection
