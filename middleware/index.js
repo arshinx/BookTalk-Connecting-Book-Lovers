9ec418a9ff98c79c28efdcbe47bd189b82e36483
Arshin Jain <arshin.jain@outlook.com>
2016-08-31 19:34:39 -0700

    Complete Logged In Middleware - Complete with Major Bug Fixes

--- middleware/index.js
+++ middleware/index.js
@@ -17,7 +17,7 @@ function loggedOut(req, res, next) {
 function requiresLogin(req, res, next) {
 
   // User logged in?
-  if (re.session && req.session.userId) {
+  if (req.session && req.session.userId) {
     // continue to next piece of middleware
     return next();
   }
