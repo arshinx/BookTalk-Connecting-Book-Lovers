2d8dc9bead5e918077fccf377642e901ae73c5c2
Arshin Jain <arshin.jain@outlook.com>
2016-08-31 19:53:00 -0700

    Major Naming Bug Fix - Connect Mongo module import

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
-var MongoStore = require('mongo-connect')(session);
+var MongoStore = require('connect-mongo')(session);
 var app = express();
 
 // MongoDB Connection
