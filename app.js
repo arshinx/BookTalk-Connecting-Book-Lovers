a81ea2bf5932b57ace0cc4eaaca67cc6c1eb0b5b
Arshin Jain <arshin.jain@outlook.com>
2016-08-31 19:44:56 -0700

    Comments and Connect-mongo

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+// Handles connections and configurations in App ---
 
+// Require Modules / Middlewares
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
