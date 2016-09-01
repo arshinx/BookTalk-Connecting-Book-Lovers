58996982f1b211f139b8e113016f21e6e685d52a
Arshin Jain <arshin.jain@outlook.com>
2016-08-31 19:18:11 -0700

    No Action if user not logged in

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,13 @@
 // Prevent Logged Out Users from Accessing a Route
 
 function loggedOut(req, res, next) {
+
+  // User logged in?
   if (req.session && req.session.userId) {
-    
+    // Send to Profile Page
+    return res.redirect('/profile');
   }
+
+  // User not logged in - no action
+  return next();
 }
